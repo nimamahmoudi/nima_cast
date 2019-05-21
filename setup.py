5a6fd0eb28cd38b369438736880c836abc691158
Nima Mahmoudi <nima.mahmoudi72@gmail.com>
2019-05-20 18:32:45 -0600

    cleaned up a bit

--- setup.py
+++ setup.py
@@ -25,8 +25,6 @@ setuptools.setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
